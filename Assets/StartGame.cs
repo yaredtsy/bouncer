101837578e41ed68c9deaaac014c0568c5bb0458
toledit <tolegpele@gmail.com>
2025-08-13 05:21:39 +0300

    New UI and Splash screen scene UI. Game UI next

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -5,9 +5,22 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public GameObject menuCanvas, mainMenu, optionsMenu, selectLevel, Logo, Tree1, Tree2;
+    public GameObject crossFade;
+    private Animator animator;
 
+    private void Awake()
+    {
+        if(menuCanvas != null)
+        menuCanvas.SetActive(false);
+    }
     void Start()
     {
+        if (crossFade != null)
+            animator = crossFade.GetComponentInChildren<Animator>();
+        selectLevel.SetActive(false);
+        optionsMenu.SetActive(false);
+
         if (videoPlayer == null)
         {
             Debug.LogError("VideoPlayer not assigned! Please assign it in the Inspector.");
@@ -17,6 +30,54 @@ public class StartGame : MonoBehaviour
     }
     private void OnVideoFinished(VideoPlayer vp)
     {
+        videoPlayer.gameObject.SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+
+    public void onPlayClicked()
+    {
+        if(animator != null)
+            animator.SetTrigger("StartFade");
+
+        Logo.SetActive(false);
+        Tree1.SetActive(false);
+        Tree2.SetActive(false);
+        mainMenu.SetActive(false);
+        selectLevel.SetActive(true);
+    }
+
+    public void OnBackClicked()
+    {
+        if (animator != null)
+            animator.SetTrigger("StartFade");
+
+        Logo.SetActive(true);
+        Tree1.SetActive(true);
+        Tree2.SetActive(true);
+        selectLevel.SetActive(false);
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void onLevelClicked()
+    {
         SceneManager.LoadScene(1);
+    }
+
+    public void onOptionsClicked()
+    {
+        if (animator != null)
+            animator.SetTrigger("StartFade");
+
+        Logo.SetActive(false);
+        Tree1.SetActive(false);
+        Tree2.SetActive(false);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void onQuitClicked()
+    {
+        Application.Quit();
     }
 }
